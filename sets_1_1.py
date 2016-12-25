5b1218931668f4e821c367c4cca39b9825a1af89
Guinsly Mondésir <guinslym@gmail.com>
2016-12-25 11:39:06 -0500

    set 1

--- sets_1_1.py
+++ sets_1_1.py
@@ -1,15 +1,21 @@
 #python 3
 import base64, binascii
 
-#Binary data of hexadecimal representation
-#help(binascii.a2b_hex)
-cipher = binascii.unhexlify(b'49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d')
+def challenge_1(string=b'49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'):
+	#Binary data of hexadecimal representation
+	#help(binascii.a2b_hex)
+	cipher = binascii.unhexlify(string)
 
-print(cipher)
-# b"I'm killing your brain like a poisonous mushroom"
+	print(cipher)
+	# b"I'm killing your brain like a poisonous mushroom"
 
-#must be a binary
-type(cipher)
+	#must be a binary
+	type(cipher)
 
-base64.b64encode(cipher)
-#SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t
+	answer = base64.b64encode(cipher)
+	#SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t
+
+	return answer
+
+a = challenge_1(b'49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d')
+print(a)
---
+++ tests_sets_1_1.py
@@ -0,0 +1,8 @@
+#python 3
+import pytest 
+from sets_1_1 import challenge_1
+
+
+def test_challenge_1():
+	answer = challenge_1(b'49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d')
+	assert(answer == b'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t')
